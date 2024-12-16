84235eb418de9ad59d27dfd48e2e7c993d66dd86
Fatma Birel <fatmabrl11@gmail.com>
2024-12-16 22:06:33 +0300

    Configure AutoMapper mappings for all entities

---
+++ WebAPI/Mapping/AboutMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DtoLayer.AboutDto;
+using EntityLayer.Entities;
+
+namespace WebAPI.Mapping
+{
+    public class AboutMapping : Profile
+    {
+        public AboutMapping()
+        {
+            CreateMap<About, ResultAboutDto>().ReverseMap();
+            CreateMap<About, GetAboutDto>().ReverseMap();
+            CreateMap<About, CreateAboutDto>().ReverseMap();
+            CreateMap<About, UpdateAboutDto>().ReverseMap();
+        }
+    }
+}
