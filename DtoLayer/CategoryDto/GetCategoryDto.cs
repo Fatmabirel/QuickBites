503a2ee752e4855470b021109faf26571f3ec21e
Fatma Birel <fatmabrl11@gmail.com>
2024-12-16 21:57:04 +0300

    Add create, result, get, and update DTOs for all entities

---
+++ DtoLayer/CategoryDto/GetCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DtoLayer.CategoryDto
+{
+    public class GetCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ DtoLayer/ProductDto/GetProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DtoLayer.ProductDto
+{
+    public class GetProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
