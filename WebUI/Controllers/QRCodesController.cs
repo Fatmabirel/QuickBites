ef24e03babf53f8e2d86b8bb13eb6ec46027e47e
Fatma Birel <fatmabrl11@gmail.com>
2025-01-04 14:39:25 +0300

    Implemented QR code generation functionality with dynamic input handling

---
+++ WebUI/Controllers/QRCodesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace WebUI.Controllers
+{
+    public class QRCodesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string value)
+        {            
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+                QRCodeGenerator.QRCode squareCode = qRCodeGenerator.CreateQrCode(value,QRCodeGenerator.ECCLevel.Q);
+                using(Bitmap image = squareCode.GetGraphic(10))
+                {
+                    image.Save(memoryStream,ImageFormat.Png);
+                    ViewBag.QrCodeImage = "data:image/png;base64," + Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+            return View();
+        }
+    }
+}
