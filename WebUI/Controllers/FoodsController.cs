6f941fa70695ec4c354b09ff7b4ef3cd604655ce
Fatma Birel <fatmabrl11@gmail.com>
2025-01-06 14:23:55 +0300

    Add functionality to fetch and display recipe data from the Tasty API by RapidAPI

---
+++ WebUI/Controllers/FoodsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.Dtos.FoodDtos;
+
+namespace WebUI.Controllers
+{
+    public class FoodsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes"),
+                Headers =
+                {
+                    { "x-rapidapi-key", "1cd0b07e55msh766ca1b28cc97eep1ddf20jsn394261c6dd7d" },
+                    { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var root = JsonConvert.DeserializeObject<FoodTastyApi>(body);
+                var values = root.Results;
+                return View(values.ToList());
+            }
+        }
+    }
+}
---
+++ WebUI/Dtos/FoodDtos/ResultFoodDto.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace WebUI.Dtos.FoodDtos
+{
+    public class FoodTastyApi
+    {
+        public List<ResultFoodDto> Results { get; set; }
+    }
+    public class ResultFoodDto
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("video_url")]
+        public string VideoUrl { get; set; }
+
+        [JsonProperty("thumbnail_url")]
+        public string ThumbnailUrl { get; set; }
+
+        [JsonProperty("total_time_minutes")]
+        public int TotalTime { get; set; }
+    }
+}
